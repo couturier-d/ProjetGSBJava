29d2a2cf97c77e77a9facba83ec41ee89dba164f
couturier-d <couturier-d@ACR-M4610G-317.tec.saint-louis.local>
2015-03-26 08:12:41 +0100

    Version avec liste vide

--- ProjetGSBJava/src/com/test/FicheFraisTest.java
+++ ProjetGSBJava/src/com/test/FicheFraisTest.java
@@ -6,19 +6,39 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.metier.FicheFrais;
+
 public class FicheFraisTest {
 
+	FicheFrais uneFicheFrais;
+	
 	@Before
 	public void setUp() throws Exception {
+		uneFicheFrais = new FicheFrais(201503, "a17");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		uneFicheFrais = null;
 	}
 
+	//test de l'instanciation
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFicheFrais() {
+		assertNotNull("L'instance est créée", uneFicheFrais);
+	}
+	
+	//test accesseur mois
+	@Test
+	public void testGetMois() {
+		assertEquals("Test accesseur mois", uneFicheFrais.getMois(), 201503);
+	}
+	
+	//test mutateur mois
+	@Test
+	public void testGetIdVisiteur() {
+		assertEquals("Test accesseur idVisiteur", uneFicheFrais.getIdVisiteur(), "a17");
 	}
 
+	
 }
