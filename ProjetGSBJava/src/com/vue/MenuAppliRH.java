c5cd197a6069a2156d3efdc5404c7e1b1c5be06c
Couturier-d <Couturier-d@192.168.1.35>
2015-05-25 13:50:07 +0200

    Ajout vue ListeUtilisateur

--- ProjetGSBJava/src/com/vue/MenuAppliRH.java
+++ ProjetGSBJava/src/com/vue/MenuAppliRH.java
@@ -12,20 +12,13 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 
 import com.metier.Utilisateur;
 import com.persistance.AccesData;
 
 import javax.swing.JLabel;
-import javax.swing.BoxLayout;
 import javax.swing.SwingConstants;
-import java.awt.Component;
-import org.eclipse.wb.swing.FocusTraversalOnArray;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
-import java.awt.CardLayout;
 
 public class MenuAppliRH extends JFrame {
 
@@ -36,8 +29,6 @@ public class MenuAppliRH extends JFrame {
 	private JMenuItem mntmListeUtilisateur;
 	private JMenuItem mntmAjoutUtilisateur;
 	private JPanel contentPane;
-	private JPanel panel_header;
-	private JPanel panel_content;
 	private JLabel lblLogoGsb;
 	
 	Image logoGsb;
@@ -49,9 +40,10 @@ public class MenuAppliRH extends JFrame {
 	
 	List<Utilisateur> lesUtilisateurs;
 	private JLabel lblWelcome;
-	private JPanel panel_welcome;
-	private JPanel panel_logo;
-	private JPanel panel_principal;
+	private JPanel sousPanel_contenuAppli;
+	private JPanel panel;
+	private JPanel sousPanel_entete;
+	private JPanel sousPanel_contenu;
 	
 
 	/**
@@ -76,9 +68,8 @@ public class MenuAppliRH extends JFrame {
 	public MenuAppliRH() {
 		setTitle("GSB | Gestion des ressources humaines");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 547, 361);
+		setBounds(100, 100, 1152, 660);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
@@ -123,42 +114,31 @@ public class MenuAppliRH extends JFrame {
 		prenomUtilisateur = "Pierre-Yves";
 		nomUtilisateur = "Halleguen";
 		estConnecte = true;
+		
+		sousPanel_contenuAppli = new JPanel();
+		contentPane.add(sousPanel_contenuAppli, BorderLayout.CENTER);
+		sousPanel_contenuAppli.setLayout(new BorderLayout(0, 0));
 		logoGsb = new ImageIcon(this.getClass().getResource("/gsbIcon.png")).getImage();
 		
-		panel_principal = new JPanel();
-		contentPane.add(panel_principal, BorderLayout.CENTER);
-		panel_principal.setLayout(new BorderLayout(0, 0));
-		
-		// Panel d'entête
-		panel_header = new JPanel();
-		panel_principal.add(panel_header, BorderLayout.NORTH);
-		panel_header.setLayout(new BorderLayout(0, 0));
+		sousPanel_entete = new JPanel();
+		sousPanel_contenuAppli.add(sousPanel_entete, BorderLayout.NORTH);
+		sousPanel_entete.setLayout(new BorderLayout(0, 0));
 		
 		lblLogoGsb = new JLabel("");
+		sousPanel_entete.add(lblLogoGsb, BorderLayout.NORTH);
 		lblLogoGsb.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_header.add(lblLogoGsb, BorderLayout.NORTH);
 		lblLogoGsb.setVerticalAlignment(SwingConstants.TOP);
 		lblLogoGsb.setIcon(new ImageIcon(logoGsb));
 		
 		lblWelcome = new JLabel("Welcome, utilisateur");
+		sousPanel_entete.add(lblWelcome, BorderLayout.CENTER);
 		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_header.add(lblWelcome, BorderLayout.SOUTH);
 		lblWelcome.setVerticalAlignment(SwingConstants.BOTTOM);
 		
-		// Panel de contenu
-		panel_content = new JPanel();
-		panel_principal.add(panel_content, BorderLayout.SOUTH);
+		sousPanel_contenu = new JPanel();
+		sousPanel_contenuAppli.add(sousPanel_contenu, BorderLayout.CENTER);
 		
-		panel_welcome = new JPanel();
-		panel_content.add(panel_welcome);
-		panel_welcome.setAlignmentY(Component.BOTTOM_ALIGNMENT);
-		
-		panel_logo = new JPanel();
-		panel_content.add(panel_logo);
-		panel_logo.setLayout(new BoxLayout(panel_logo, BoxLayout.X_AXIS));
-		panel_header.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{panel_logo, lblLogoGsb, panel_welcome, lblWelcome}));
-		
-		if (estConnecte) {
+		if(estConnecte){
 			lblWelcome.setText("Bienvenue, " + prenomUtilisateur + " " + nomUtilisateur);
 		}
 		
@@ -180,15 +160,19 @@ public class MenuAppliRH extends JFrame {
 	// Pannels pour secrétaire
 	
 	private void affichageListeUtilisateurs() {
-//		this.setContentPane(new PanelListeUtilisateur(lesUtilisateurs));
+		sousPanel_contenuAppli.remove(sousPanel_contenu);
+		sousPanel_contenu = new JPanel();
+		sousPanel_contenuAppli.add(sousPanel_contenu, BorderLayout.CENTER);
+		sousPanel_contenu.add(new PanelListeUtilisateurs(lesUtilisateurs), BorderLayout.CENTER);
 		this.revalidate();
-		System.out.println("affichageListeUtilisateurs");
 	}
 	
 	private void affichageAjoutUtilisateur() {
-//		this.setContentPane(new PanelAjoutUtilisateur());
+		sousPanel_contenuAppli.remove(sousPanel_contenu);
+		sousPanel_contenu = new JPanel();
+		sousPanel_contenuAppli.add(sousPanel_contenu, BorderLayout.CENTER);
+		sousPanel_contenu.add(new PanelAjoutUtilisateur(), BorderLayout.CENTER);
 		this.revalidate();
-		System.out.println("affichageAjoutUtilisateur");
 	}
 	
 	/*private void affichageModifUtilisateur(Utilisateur unUtilisateur) {
--- ProjetGSBJava/src/com/vue/PanelListeUtilisateurs.java
+++ ProjetGSBJava/src/com/vue/PanelListeUtilisateurs.java
@@ -1,15 +1,167 @@
 package com.vue;
 
 import javax.swing.JPanel;
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.border.LineBorder;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+
+import com.metier.Region;
+import com.metier.Utilisateur;
+import com.modele.ModeleUtilisateur;
+import com.persistance.AccesData;
+
+import javax.swing.JButton;
+
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SwingConstants;
 
 public class PanelListeUtilisateurs extends JPanel {
-	public PanelListeUtilisateurs() {
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		
-		
-	}
+	private JScrollPane scrollPane;
+	private JTable table;
+	public ModeleUtilisateur modele;
+	private JComboBox<String> cbxFiltreUtilisateur_region;
+	private JTextField txtSearchUtilisateur_id;
+	private JTextField txtSearchUtilisateur_nom;
+	private JTextField txtSearchUtilisateur_prenom;
 	
+	private JLabel lblFiltreUtilisateur_region, lblFiltreUtilisateur_id, lblFiltreUtilisateur_nom, lblFiltreUtilisateur_prenom;
+	
+	private List<Utilisateur> listeUtilisateurs;
+	private List<Region> listeRegions;
+	
+	public PanelListeUtilisateurs(List<Utilisateur> lesUtilisateurs) {
+		setBorder(new LineBorder(new Color(0, 0, 0)));
+		setLayout(new BorderLayout(0, 0));
+		setPreferredSize(new Dimension(1024, 512));
+		
+		listeUtilisateurs = lesUtilisateurs;
+		listeRegions = AccesData.getListeRegion();
+		
+		modele = new ModeleUtilisateur(listeUtilisateurs);
+		
+		// Gestion des filtres de recherche
+		
+		JPanel sousPanel_contenuUtilisateurs = new JPanel();
+		sousPanel_contenuUtilisateurs.setForeground(Color.BLACK);
+		add(sousPanel_contenuUtilisateurs, BorderLayout.CENTER);
+		sousPanel_contenuUtilisateurs.setLayout(new BorderLayout(0, 0));
+		
+		JPanel sousPanel_searchUtilisateur = new JPanel();
+		sousPanel_searchUtilisateur.setForeground(Color.BLACK);
+		sousPanel_contenuUtilisateurs.add(sousPanel_searchUtilisateur, BorderLayout.NORTH);
+		sousPanel_searchUtilisateur.setLayout(new BorderLayout(0, 0));
+		
+		// Entête du panel
+		
+		JPanel sousPanel_searchUtilisateur_entete = new JPanel();
+		sousPanel_searchUtilisateur.add(sousPanel_searchUtilisateur_entete, BorderLayout.NORTH);
+		
+		JLabel lblALaRecherche = new JLabel("A la recherche d'un visiteur particulier ?");
+		sousPanel_searchUtilisateur_entete.add(lblALaRecherche);
+		
+		// Filtres de recherche
+		
+		JPanel sousPanel_searchUtilisateur_filtres = new JPanel();
+		sousPanel_searchUtilisateur.add(sousPanel_searchUtilisateur_filtres, BorderLayout.CENTER);
+		
+		lblFiltreUtilisateur_region = new JLabel("R\u00E9gion :");
+		sousPanel_searchUtilisateur_filtres.add(lblFiltreUtilisateur_region);
+		
+		cbxFiltreUtilisateur_region = new JComboBox<String>();
+		sousPanel_searchUtilisateur_filtres.add(cbxFiltreUtilisateur_region);
+//		cbxFiltreUtilisateur_region.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				if (cbxFiltreUtilisateur_region.getSelectedIndex()!= -1) 	{
+////				String idRegion = listeRegions.get(cbxFiltreUtilisateur_region.getSelectedIndex()).getIdRegion();
+////						! ! ! Récupération des utilisateurs d'une région ! ! !
+////				listeUtilisateurs = AccesData.getListeUtilisateur();
+//				//modele = new ModeleUtilisateur(listeUtilisateurs);
+////				table.setModel(modele);
+//				 // réactualisation de la table
+////				table.revalidate();
+//				
+//				}
+//			}
+//		});
+		
+		for(Region r: listeRegions)
+		{
+			cbxFiltreUtilisateur_region.addItem(r.getNomRegion());
+		}
+		
+		cbxFiltreUtilisateur_region.setSelectedIndex(0);
+		
+		lblFiltreUtilisateur_id = new JLabel("ID :");
+		sousPanel_searchUtilisateur_filtres.add(lblFiltreUtilisateur_id);
+		
+		txtSearchUtilisateur_id = new JTextField();
+		sousPanel_searchUtilisateur_filtres.add(txtSearchUtilisateur_id);
+		txtSearchUtilisateur_id.setColumns(4);
+		
+		lblFiltreUtilisateur_nom = new JLabel("Nom :");
+		sousPanel_searchUtilisateur_filtres.add(lblFiltreUtilisateur_nom);
+		
+		txtSearchUtilisateur_nom = new JTextField();
+		sousPanel_searchUtilisateur_filtres.add(txtSearchUtilisateur_nom);
+		txtSearchUtilisateur_nom.setColumns(10);
+		
+		// Champ de recheerche par prénom
+		
+		lblFiltreUtilisateur_prenom = new JLabel("Prenom :");
+		sousPanel_searchUtilisateur_filtres.add(lblFiltreUtilisateur_prenom);
+		
+		txtSearchUtilisateur_prenom = new JTextField();
+		sousPanel_searchUtilisateur_filtres.add(txtSearchUtilisateur_prenom);
+		txtSearchUtilisateur_prenom.setColumns(10);
+		
+		// Boutons de validation de la recherche
+		
+		JPanel sousPanel_searchUtilisateur_bouttons = new JPanel();
+		sousPanel_searchUtilisateur.add(sousPanel_searchUtilisateur_bouttons, BorderLayout.SOUTH);
+		
+		JButton btnSearchUtilisateur_id = new JButton("Rechercher par ID");
+		sousPanel_searchUtilisateur_bouttons.add(btnSearchUtilisateur_id);
+		
+		JButton btnSearchUtilisateur_nomPrenom = new JButton("Rechercher par nom prenom");
+		sousPanel_searchUtilisateur_bouttons.add(btnSearchUtilisateur_nomPrenom);
+		
+		JPanel sousPanel_listeUtilisateur = new JPanel();
+		sousPanel_listeUtilisateur.setForeground(Color.BLACK);
+		sousPanel_contenuUtilisateurs.add(sousPanel_listeUtilisateur, BorderLayout.CENTER);
+		sousPanel_listeUtilisateur.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblRsultatDeLa = new JLabel("R\u00E9sultat de la recherche :");
+		lblRsultatDeLa.setHorizontalAlignment(SwingConstants.CENTER);
+		sousPanel_listeUtilisateur.add(lblRsultatDeLa, BorderLayout.NORTH);
+		sousPanel_listeUtilisateur.add(getScrollPane());
+	}		
+	
+	public JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(54, 121, 768, 256);
+			scrollPane.setViewportView(getTable());
+		}
+		return scrollPane;
+	}
+	public JTable getTable() {
+		if (table == null) {
+			table = new JTable(modele);
+			table.setSize(1024, 512);
+		}
+		return table;
+	}	
 }
